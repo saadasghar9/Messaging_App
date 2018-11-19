8c583dab920085e3fa8db018ae1bba04b3af357a
Your Name <you@example.com>
2018-11-19 21:20:55 +0500

    the app for messaging under learning of devslopes

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,41 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Jonny B on 7/18/17.
+//  Copyright © 2017 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel : Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+        channelName.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        
+        for id in MessageService.instance.unreadChannels {
+            if id == channel.id {
+                channelName.font = UIFont(name: "HelveticaNeue-Bold", size: 22)
+            }
+        }
+    }
+}
